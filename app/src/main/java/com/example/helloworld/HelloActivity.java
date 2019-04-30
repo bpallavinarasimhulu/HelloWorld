d57327bd662c60099b41cf7306ad89ad59201430
bpallavinarasimhulu <bpallavi068@gmail.com>
2019-04-30 12:44:15 +0530

    this project is about just moving from one activity to another activity

---
+++ app/src/main/java/com/example/helloworld/HelloActivity.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloActivity extends AppCompatActivity {
+       TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+        textView=findViewById(R.id.textView2);
+    }
+}
---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et;
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et=findViewById(R.id.editText2);
+        button=findViewById(R.id.click);
+    }
+
+    public void button(View view) {
+        Intent intent=new Intent(this,HelloActivity.class);
+        startActivity(intent);
+
+    }
+}
